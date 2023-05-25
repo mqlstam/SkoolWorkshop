5ca31e64b3a458122ac66aa6a6ea09b343cfbc1b
mqlstam <118191161+mqlstam@users.noreply.github.com>
2023-05-25 23:09:58 +0200

    feat: add WorkshopForm.vue with template data

---
+++ frontend/src/views/WorkshopForm.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { reactive } from 'vue';
+
+const workshop = reactive({
+  name: '',
+  description: '',
+  // Add other fields as necessary
+});
+
+function submitForm() {
+  // Handle form submission here
+}
+</script>
+
+
+<template> 
+    <form @submit.prevent="submitForm">
+      <div>
+        <label for="workshop-name">Workshop Name</label>
+        <input id="workshop-name" v-model="workshop.name" required />
+      </div>
+  
+      <div>
+        <label for="workshop-description">Workshop Description</label>
+        <input id="workshop-description" v-model="workshop.description" required />
+      </div>
+  
+      <!-- Add other fields as necessary -->
+  
+      <button type="submit">Create Workshop</button>
+    </form>
+  </template>
