7f485e351561ed0ae1ba53bd1b24909d3b5ff6c3
Job79 <job@plabble.org>
2023-05-25 13:32:22 +0200

    feat: add prisma and implement GET products / workshops

--- backend/controller/WorkshopController.js
+++ backend/controller/WorkshopController.js
@@ -1,11 +1,18 @@
 export class WorkshopController {
+    constructor(prisma) {
+        this.prisma = prisma
+    }
+
     /**
      * GET /workshops
      */
-    get(req, res) {
-        res.status(200).send([
-            {name: "Graffiti", groupSize: 25},
-            {name: "Graffiti", groupSize: 25}
-        ])
+    async get(req, res) {
+        const workshops = await this.prisma.workshop.findMany()
+        if(!workshops) {
+            res.status(404).send({message: 'No workshops found'})
+            return
+        }
+
+        res.status(200).send(workshops)
     }
 }
--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,14 @@ dotenv.config()
 import express from 'express'
 import {WorkshopController} from "./controller/WorkshopController.js"
 import {ProductController} from './controller/ProductController.js'
+import {PrismaClient} from "@prisma/client";
 
 const app = express()
 const port = 3000
 
-const workshopController = new WorkshopController()
-const productController = new ProductController()
+const prisma = new PrismaClient()
+const workshopController = new WorkshopController(prisma)
+const productController = new ProductController(prisma)
 
 app.get('/api/workshops', (req, res) => workshopController.get(req, res))
    .get('/api/products', (req, res) => productController.get(req, res))
