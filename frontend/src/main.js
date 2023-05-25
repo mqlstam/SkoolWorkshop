9cecf36113497d23d0a114d4324295e14100c9d1
rikoldebijvank <r.oldebijvank@student.avans.nl>
2023-05-25 11:55:26 +0200

    Bootstrap added

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import {createPinia} from "pinia";
 import router from "./router/router.js"
+// Import our custom CSS
+import './scss/styles.scss'
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
 
 createApp(App).use(createPinia()).use(router).mount('#app')
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
   server: {
     proxy: {
       '/api': {target: 'http://localhost:3000'}
