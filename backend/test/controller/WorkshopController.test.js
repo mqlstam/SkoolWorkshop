1929424f4dd5b652e01ab7621f9435526e6e9f46
mqlstam <118191161+mqlstam@users.noreply.github.com>
2023-05-28 01:48:17 +0200

    feat: add submitForm tests

--- backend/test/controller/WorkshopController.test.js
+++ backend/test/controller/WorkshopController.test.js
@@ -32,4 +32,49 @@ describe('WorkshopController', () => {
             expect(res.send.calledOnceWith({ message: 'No workshops found' })).to.be.true
         })
     })
+
+    describe('post', () => {
+        it('should post a new workshop', async () => {
+            const req = {
+                body: { name: 'Workshop 3', groupSize: 15 }
+            }
+            const res = { status: sinon.stub().returnsThis(), send: sinon.stub() }
+            const db = { workshop: { findUnique: sinon.stub().returns(null), post: sinon.stub().returns(req.body) } }
+            const controller = new WorkshopController(db)
+
+            await controller.post(req, res)
+            expect(db.workshop.findUnique.calledOnce).to.be.true
+            expect(db.workshop.post.calledOnce).to.be.true
+            expect(res.status.calledOnceWith(201)).to.be.true
+            expect(res.send.calledOnceWith(req.body)).to.be.true
+        })
+
+        it('should return 400 if workshop name is already taken', async () => {
+            const req = {
+                body: { name: 'Workshop 1', groupSize: 15 }
+            }
+            const res = { status: sinon.stub().returnsThis(), send: sinon.stub() }
+            const db = { workshop: { findUnique: sinon.stub().returns(workshops[0]), post: sinon.stub() } }
+            const controller = new WorkshopController(db)
+
+            await controller.post(req, res)
+            expect(db.workshop.findUnique.calledOnce).to.be.true
+            expect(db.workshop.post.called).to.be.false
+            expect(res.status.calledOnceWith(400)).to.be.true
+            expect(res.send.calledOnceWith({ message: `Workshop with name ${req.body.name} already exists.` })).to.be.true
+        })
+
+        it('should return 400 if workshop name or groupSize is missing', async () => {
+            const req = { body: {} }
+            const res = { status: sinon.stub().returnsThis(), send: sinon.stub() }
+            const db = { workshop: { findUnique: sinon.stub(), post: sinon.stub() } }
+            const controller = new WorkshopController(db)
+
+            await controller.post(req, res)
+            expect(db.workshop.findUnique.called).to.be.false
+            expect(db.workshop.post.called).to.be.false
+            expect(res.status.calledOnceWith(400)).to.be.true
+            expect(res.send.calledOnceWith({ message: 'Workshop name and groupSize are required.' })).to.be.true
+        })
+    })
 })
