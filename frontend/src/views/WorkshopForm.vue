45ab78890a74b1b6c37d360a129b08c931cd07d0
mqlstam <118191161+mqlstam@users.noreply.github.com>
2023-05-29 13:01:13 +0200

    feat: changed script to use workshopstore

--- frontend/src/views/WorkshopForm.vue
+++ frontend/src/views/WorkshopForm.vue
@@ -1,39 +1,11 @@
 <script setup>
-import { reactive } from 'vue'
+import { useWorkshopStore } from '../store/workshopStore.js'
 
-const workshop = reactive({
-    name: '',
-    groupSize: null
-})
-
-async function submitForm () {
-    const response = await fetch('/api/workshops', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(workshop)
-    })
-
-    if (!response.ok) {
-        const message = await response.text()
-        console.error(`Error creating workshop: ${message}`)
-        return
-    }
-
-    const createdWorkshop = await response.json()
-
-    // Clear the form
-    workshop.name = ''
-    workshop.groupSize = ''
-
-    // Here you could also add any code to handle the created workshop
-    console.log(`Created workshop: ${JSON.stringify(createdWorkshop)}`)
-}
+const workshop = useWorkshopStore()
 </script>
 
 <template>
-  <form @submit.prevent="submitForm">
+  <form @submit.prevent="workshop.submitForm">
     <div>
       <label for="workshop-name">Workshop Name</label>
       <input id="workshop-name" v-model="workshop.name" required />
@@ -41,9 +13,8 @@ async function submitForm () {
 
     <div>
       <label for="group-size">Group Size</label>
-      <input id="group-size" v-model="workshop.groupSize" type="number" min="1" required />
+      <input id="group-size" v-model.number="workshop.groupSize" type="number" min="1" required />
     </div>
-    <!-- Add other fields as necessary -->
 
     <button type="submit">Create Workshop</button>
   </form>
