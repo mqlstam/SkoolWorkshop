25caf0732cab4d6645b94cf79b0a9b5d85cb2dd4
Job79 <job@plabble.org>
2023-05-31 14:47:30 +0200

    fix: use router.back() instead of <a> for going back

--- frontend/src/component/workshop/WorkshopInfo.vue
+++ frontend/src/component/workshop/WorkshopInfo.vue
@@ -2,8 +2,8 @@
 import { useWorkshopStore } from '../../store/workshopStore.js'
 import NumberInput from '../input/NumberInput.vue'
 import TextInput from '../input/TextInput.vue'
+import {useRouter} from "vue-router";
 
-const workshopStore = useWorkshopStore()
 const props = defineProps({
     workshopId: {
         type: Number,
@@ -11,6 +11,8 @@ const props = defineProps({
     }
 })
 
+const router = useRouter()
+const workshopStore = useWorkshopStore()
 const workshop = await workshopStore.get(props.workshopId)
 
 async function save () {
@@ -25,7 +27,7 @@ async function save () {
       <text-input name="name" v-model:value="workshop.name" @update:value="save" placeholder="name"/>
       <number-input name="group size" v-model:value="workshop.groupSize" @update:value="save" placeholder="group size" />
 
-      <a type="button" class="mt-3 mb-4 btn btn-secondary btn-lg w-50" href="/workshops">Back</a>
+      <a @click="router.back()" type="button" class="mt-3 mb-4 btn btn-secondary btn-lg w-50">Back</a>
     </form>
   </div>
 </template>
