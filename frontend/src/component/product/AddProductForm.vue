97d4ca548db236bcac14ee993a8c40ebd3df614e
kgithcoy <anhtuannguyen.2004@gmail.com>
2023-06-01 14:54:46 +0200

    fixed spelling errors

--- frontend/src/component/product/AddProductForm.vue
+++ frontend/src/component/product/AddProductForm.vue
@@ -23,9 +23,9 @@ async function create () {
 <template>
     <div class="row box-md bg-white border-top">
       <form class="col-12" @submit.prevent="create">
-        <text-input name="name" v-model="product.name" />
-        <number-input name="stock" v-model="product.stock" />
-        <number-input name="minStock" v-model="product.minStock" />
+        <text-input name="Name" v-model="product.name" />
+        <number-input name="Stock" v-model="product.stock" />
+        <number-input name="Minimum Stock" v-model="product.minStock" />
 
         <div class="d-flex justify-content-between mt-3">
           <a type="button" class="btn btn-secondary btn-lg w-50 me-2" href="/products">Back</a>
--- frontend/src/component/product/EditProductForm.vue
+++ frontend/src/component/product/EditProductForm.vue
@@ -22,9 +22,9 @@ async function save () {
 <template>
   <div class="row box-md bg-white border-top">
     <form class="col-12">
-      <text-input name="name" v-model:modelValue="product.name" @update:modelValue="save"/>
-      <number-input name="stock" v-model:modelValue="product.stock" @update:modelValue="save"  />
-      <number-input name="minStock" v-model:modelValue="product.minStock" @update:modelValue="save"/>
+      <text-input name="Name" v-model:modelValue="product.name" @update:modelValue="save"/>
+      <number-input name="Stock" v-model:modelValue="product.stock" @update:modelValue="save"  />
+      <number-input name="Minimum Stock" v-model:modelValue="product.minStock" @update:modelValue="save"/>
 
       <a type="button" class="mt-3 mb-4 btn btn-secondary btn-lg w-50" href="/products">Back</a>
     </form>
