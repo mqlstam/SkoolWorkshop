307d49bfb6784d1e034be1d6edead3983b880b89
mqlstam <118191161+mqlstam@users.noreply.github.com>
2023-06-02 00:01:07 +0200

    refactor: update workshopStore.js

--- frontend/src/store/workshopStore.js
+++ frontend/src/store/workshopStore.js
@@ -3,9 +3,7 @@ import { API } from '../util/Api.js'
 
 export const useWorkshopStore = defineStore('workshop', {
     state: () => ({
-        workshops: [],
-        name: '',
-        groupSize: null
+        workshops: []
     }),
     actions: {
         async fetch () {
@@ -38,26 +36,12 @@ export const useWorkshopStore = defineStore('workshop', {
             }
         },
 
-        setName (name) {
-            this.name = name
-        },
-
-        setGroupSize (size) {
-            this.groupSize = size
-        },
-
-        reset () {
-            this.name = ''
-            this.groupSize = null
-        },
-
-        async submitForm (imageRef) {
-            console.log(`Submitting form with name ${this.name} and group size ${this.groupSize}`)
+        async post (workshopData, imageFile) {
             const formData = new FormData()
-            formData.append('name', this.name)
-            formData.append('groupSize', this.groupSize)
-            if (imageRef.value?.files[0]) {
-                formData.append('image', imageRef.value.files[0])
+            formData.append('name', workshopData.name)
+            formData.append('groupSize', workshopData.groupSize)
+            if (imageFile) {
+                formData.append('image', imageFile)
             }
 
             const response = await fetch('/api/workshops', {
@@ -67,17 +51,11 @@ export const useWorkshopStore = defineStore('workshop', {
 
             if (!response.ok) {
                 const message = await response.text()
-                console.error(`Error creating workshop: ${message}`)
-                return
+                throw new Error(message)
             }
 
             const createdWorkshop = await response.json()
-
-            // Clear the form
-            this.reset()
-
-            // Here you could also add any code to handle the created workshop
-            console.log(`Created workshop: ${JSON.stringify(createdWorkshop)}`)
+            this.workshops.push(createdWorkshop)
         }
     }
 })
