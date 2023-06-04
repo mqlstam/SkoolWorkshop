c5c642f8fba55e386040d185d3653861e41e5e2d
rikoldebijvank <r.oldebijvank@student.avans.nl>
2023-06-04 11:07:45 +0200

    pop up created

---
+++ frontend/src/component/scanner/CodeScanner.vue
@@ -0,0 +1,15 @@
+<script setup>
+import {StreamBarcodeReader} from "vue-barcode-reader"
+
+function onDecode(result) {
+  console.log(result)
+}
+</script>
+
+<template>
+  <div class="scannerPopUp position-absolute centered-rectangle w-auto h-auto mt-4 border-5">
+    <div class="row w-50">
+      <stream-barcode-reader @decode="onDecode" size="100px"></stream-barcode-reader>
+    </div>
+  </div>
+</template>
