41540b67274562b64abf16c5a61515ab44aaecf5
kgithcoy <anhtuannguyen.2004@gmail.com>
2023-06-04 16:17:20 +0200

    refactor: fixed bug in workshop overview page

--- frontend/src/component/workshop/WorkshopItem.vue
+++ frontend/src/component/workshop/WorkshopItem.vue
@@ -4,44 +4,44 @@ import { useRouter } from 'vue-router'
 
 const emit = defineEmits(['click', 'delete'])
 const props = defineProps({
-    workshop: {
-        type: Object,
-        required: true
-    },
-    edit: {
-        type: Boolean,
-        default: false
-    }
+  workshop: {
+    type: Object,
+    required: true
+  },
+  edit: {
+    type: Boolean,
+    default: false
+  }
 })
 
 const router = useRouter()
 
 const handleClick = (workshop) => {
-    emit('click', workshop)
-    const route = props.edit ? `/workshops/${workshop.id}` : `/workshops/${workshop.id}/products`
-    router.push(route)
+  emit('click', workshop)
+  const route = props.edit ? `/workshops/${workshop.id}` : `/workshops/${workshop.id}/products`
+  router.push(route)
 }
 </script>
 
 <template>
-  <div role="button"
-       class="d-flex align-items-center border-bottom hover-darken"
-       @click="emit('click', props.workshop)"
-       :to="`/workshops/${props.workshop.id}`">
+  <div role="button" class="d-flex align-items-center border-bottom hover-darken" @click="handleClick(props.workshop)">
 
     <!-- image and title -->
-    <font-awesome-icon :icon="['fas', 'inbox']" class="fa-3x img border p-3 ms-1 me-3 my-3"/>
+    <font-awesome-icon :icon="['fas', 'inbox']" class="fa-3x img border p-3 ms-1 me-3 my-3" />
     <span class="h5"> {{ props.workshop.name }} </span>
 
     <div v-if="!props.edit" class="ms-auto">
       <!-- workshop stock status -->
-      <font-awesome-icon :icon="['fas', 'check']" class="fa-1x rounded-circle p-1 m-4 bg-success text-white" style="width:20px;height:20px;"/>
+      <font-awesome-icon :icon="['fas', 'check']" class="fa-1x rounded-circle p-1 m-4 bg-success text-white"
+        style="width:20px;height:20px;" />
     </div>
     <div v-else class="ms-auto">
       <!-- edit mode buttons -->
-      <button class="btn p-2 hover-darken" @click.prevent="emit('delete', props.workshop)">
-        <font-awesome-icon :icon="['fas', 'trash']" class="scale-up-center fa-xl rounded-circle p-3 bg-danger text-white"/>
+      <button class="btn p-2 hover-darken" @click.stop.prevent="emit('delete', props.workshop)">
+        <font-awesome-icon :icon="['fas', 'trash']"
+          class="scale-up-center fa-xl rounded-circle p-3 bg-danger text-white" />
       </button>
+
     </div>
   </div>
 </template>
