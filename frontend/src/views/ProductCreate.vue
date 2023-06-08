f506f9fbcd826a82df45da52f2b375301794c5ea
Job79 <job@plabble.org>
2023-06-08 10:43:38 +0200

    fix: small styling changes

--- frontend/src/views/ProductCreate.vue
+++ frontend/src/views/ProductCreate.vue
@@ -19,7 +19,6 @@ const product = ref({
 
 async function create () {
     if (product.value.name === '') throw new Error('name is empty')
-    if (product.value.code === '') delete product.value.code
     await productStore.create(product.value)
     await router.back()
 }
@@ -41,9 +40,8 @@ async function create () {
   <div class="row box bg-white border-top">
     <text-input name="Name" v-model:value="product.name" />
     <number-input name="Stock" v-model:value="product.stock" />
-    <checkbox-input name="Reusable" v-model:value="product.reusable" />
-    <number-input name="Minimum Stock" v-model:value="product.minStock" />
     <scan-input v-model:value="product.code" />
+    <checkbox-input name="Reusable" v-model:value="product.reusable" />
 
     <button class="m-3 ms-auto btn p-2 bg-primary d-flex justify-content-center" @click="create" style="width: 10rem">
       <font-awesome-icon :icon="['fas', 'floppy-disk']" class="fa-xl" />
--- frontend/src/views/ProductDetails.vue
+++ frontend/src/views/ProductDetails.vue
@@ -16,7 +16,6 @@ const product = ref(await productStore.get(productId))
 
 async function save () {
     const { id, ...data } = product.value
-    if (data.code === '') delete data.code
     await productStore.update(data, id)
 }
 </script>
@@ -37,7 +36,7 @@ async function save () {
   <div class="row box bg-white border-top">
     <text-input name="Name" v-model:value="product.name" @update:value="save"/>
     <number-input name="Stock" v-model:value="product.stock" @update:value="save"/>
-    <checkbox-input name="Reusable" v-model:value="product.reusable" @update:value="save"/>
     <scan-input v-model:value="product.code" @update:value="save"/>
+    <checkbox-input name="Reusable" v-model:value="product.reusable" @update:value="save"/>
   </div>
 </template>
