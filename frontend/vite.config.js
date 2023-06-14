3e43848a47b3e96def7d928e30b0f727859d6f62
kgithcoy <anhtuannguyen.2004@gmail.com>
2023-06-14 11:56:59 +0200

    feat: add webapp intergration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,38 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [vue(), VitePWA({
+        registerType: 'autoUpdate',
+        devOptions: {
+            enabled: true
+        },
+        includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+        manifest: {
+            name: 'Warehouse',
+            short_name: 'Warehouse',
+            start_url: '/',
+            icons: [
+                {
+                    src: '/android-chrome-192x192.png',
+                    sizes: '192x192',
+                    type: 'image/png'
+                },
+                {
+                    src: '/android-chrome-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png'
+                }
+            ],
+            theme_color: '#ffffff',
+            background_color: '#ffffff',
+            display: 'standalone'
+        }
+
+    })],
     resolve: {
         alias: {
             '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap')
