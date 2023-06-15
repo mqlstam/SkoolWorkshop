6d251df75accacc63834e94988999e0b9587d286
Job79 <job@plabble.org>
2023-06-15 11:06:27 +0200

    fix: make wpa config valid

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import * as path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue(), VitePWA({
-        includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+        includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
         manifest: {
             name: 'Warehouse',
             short_name: 'Warehouse',
